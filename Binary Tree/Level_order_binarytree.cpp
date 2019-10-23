a152bab036dca05ebed1e16c5f2ae033f95b12d7
Keshav Malik <33570148+kmalik31@users.noreply.github.com>
2019-10-23 13:57:43 +0530

    Create Level_order_binarytree.cpp

---
+++ Binary Tree/Level_order_binarytree.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* left,*right;
+    Node(int data)
+    {
+        this->data = data;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+void levelorder(Node* root)
+{
+    if(root == nullptr)
+    return;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+        cout<<temp->data<<" ";
+        if(temp->left)
+        q.push(temp->left);
+        if(temp->right)
+        q.push(temp->right);
+    }
+}
+int main()
+{
+    Node* root = new Node(90);
+    root->left = new Node(10);
+    root->right = new Node(20);
+    levelorder(root);
+}
